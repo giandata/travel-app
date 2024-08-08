c01b728d6609d0e7e43c40c69c5ea8e1278a7f63
giandata <didonatogiancarlo89@gmail.com>
2024-08-08 23:58:38 +0200

    format def

--- src/v1/widget/receive_plan.py
+++ src/v1/widget/receive_plan.py
@@ -1,6 +1,6 @@
 ##TODO make a def and use a fragment
-        with st.form(key="receive_plan", clear_on_submit=True, border=False):
-            st.text_input(label="Insert your email")
-            submitted = st.form_submit_button("Receive plan")
-        if submitted:
-            st.success("check your inbox")
+with st.form(key="receive_plan", clear_on_submit=True, border=False):
+    st.text_input(label="Insert your email")
+    submitted = st.form_submit_button("Receive plan")
+if submitted:
+    st.success("check your inbox")
