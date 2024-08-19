0c9c134d7327675ea2279878ea4697bea80e39b5
giandata <didonatogiancarlo89@gmail.com>
2024-08-19 14:45:17 +0200

    added travels in init

--- src/v1/pages/Blog.py
+++ src/v1/pages/Blog.py
@@ -4,7 +4,7 @@ import os
 import sys
 
 # The following line allows using absolute imports relative to "src"
-project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), "../.."))
+project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), "../../.."))
 if project_root not in sys.path:
     sys.path.append(project_root)
 
