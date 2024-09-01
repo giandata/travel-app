76f60efe776895d27bae92aa8a2a5e5da7f4fb94
giandata <didonatogiancarlo89@gmail.com>
2024-09-01 12:36:59 +0200

    json schema and parsing

--- src/v1/pages/json response.py
+++ src/v1/pages/json response.py
@@ -19,13 +19,34 @@ if content != None:
 
     prompt = content
 
+    json_example = {
+        "itinerary_summary": "title",
+        "   itinerary": [
+            {
+                "day": "Day 1: Arrival place",
+                "description": {
+                    "activities": ["activity 1", "activity 2"],
+                    "accommodation": "accommodation example",
+                    "transportation": "transportation example",
+                },
+            }
+        ],
+        "travel_summary": {
+            "total_cost": "Approx cost €",
+            "cost_breakdown": {"example_cost_item": "€100"},
+            "additional_suggestions": ["suggestion 1", "suggestion 2"],
+            "explanation": "eventual explanations",
+        },
+    }
+
     chat_completion = client.chat.completions.create(
         model="gpt-4o-mini",
         response_format={"type": "json_object"},
         messages=[
             {
                 "role": "system",
-                "content": "You are a helpful assistant that provides responses in JSON format.",
+                "content": "You are a helpful assistant designed to output JSON. The data schema wil be like this"
+                + json.dumps(json_example),
             },
             {"role": "user", "content": prompt},
         ],
@@ -33,15 +54,25 @@ if content != None:
     # Extract the response content
     response_content = chat_completion.choices[0].message.content
 
-    # Attempt to parse the response as JSON
-    try:
-        response_json = json.loads(response_content)
-        # Display the parsed JSON response
-        st.subheader("Parsed Response")
-        st.json(response_json)
-    except json.JSONDecodeError as e:
-        st.error(f"Failed to parse JSON: {e}")
-        st.write("Raw response content:", response_content)
+    finish_reason = chat_completion.choices[0].finish_reason
 
-    except Exception as e:
-        st.error(f"An error occurred: {e}")
+    if finish_reason == "stop":
+        # Attempt to parse the response as JSON
+        try:
+            response_json = json.loads(response_content)
+            # Display the parsed JSON response
+            st.subheader("Parsed Response")
+            # full response
+            # st.json(response_json)
+
+            # example print of json chunk
+            st.json(response_json["itinerary"])
+
+        except json.JSONDecodeError as e:
+            st.error(f"Failed to parse JSON: {e}")
+            st.write("Raw response content:", response_content)
+
+        except Exception as e:
+            st.error(f"An error occurred: {e}")
+    else:
+        st.error(f"Finish reason {finish_reason}")
